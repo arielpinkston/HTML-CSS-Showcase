329ddf3dcb0676f5b326d7c409c701dad9d99e2a
arielpinkston <arielpinkston05@hotmail.com>
2024-05-17 23:00:35 -0500

    Add files via upload

---
+++ HTML and CSS Project/forms/results.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html >
+<html>
+<head>
+  <meta charset="utf-8" >
+  <title>Pinkston Hotel Services</title>
+  <style></style>
+  <link rel="stylesheet" type="text/css" href="../styles.css">
+</head>
+<body>
+  <h1>Success!</h1>
+  <p>Your information has been successfully submitted, thank you for your patience!</p>
+</body>
+<footer><a href="../index.html">Back to the Home page</a> - <a href="form.html">Submit Another RSVP</a></small></p></footer>
+        <small><p>&copy;2023 by Ariel Pinkston</p></small>
+</html>
